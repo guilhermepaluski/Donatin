847e11784205a228d4243aef61384dc25f1a334c
Guilherme Mafra Paluski <guimpaluski@gmail.com>
2026-07-29 10:17:31 -0300

    style: Added the AppBar title + Notification Icon + Search Bar field.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,20 +16,64 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: AppColors.white,
         elevation: 0,
+        toolbarHeight: 70,
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
               'Olá, Guilherme! 👋',
-              style: TextStyle(color: AppColors.black, fontSize: 22),
+              style: TextStyle(
+                color: AppColors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
             ),
+            SizedBox(height: 4),
             Text(
               'Que bom ter você aqui!',
-              style: TextStyle(color: AppColors.black, fontSize: 16),
+              style: TextStyle(
+                color: Color.fromARGB(255, 116, 116, 116),
+                fontSize: 14,
+              ),
             ),
           ],
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_none, color: AppColors.black, size: 28),
+            onPressed: () {
+              // ADICIONAR A LÓGICA DO BOTÃO DE NOTIFICAÇÕES AQUI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            },
+          ),
+          const SizedBox(width: 8),
+        ],
       ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // --- CAMPO DE BUSCA DE CAMPANHAS ---
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Buscar campanhas',
+                hintStyle: const TextStyle(color: Colors.grey, fontSize: 15),
+                prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                filled: true,
+                fillColor: const Color(0xFFF2F2F2),
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+
+            // === CATEGORIAS ===
+          ]
+        )
+      )
     );
   }
 }
